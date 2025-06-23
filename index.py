f6c5e72b4dd8eec79710d20cdad604a1f04ee6d5
HeheSmile <maiminhjay0825@gmail.com>
2025-06-24 04:16:05 +0700

    test

--- index.py
+++ index.py
@@ -44,34 +44,34 @@ with open('config.json','r', encoding = 'utf-8') as f :
 zodiac_df = df
 
 #Call LLM 
-model = genai.GenerativeModel("gemini-1.5-flash")
-    
-genai.GenerativeModel("gemini-1.5-flash",
-                            system_instruction=f"""
+@st.cache_resource
+def get_model():
+    return genai.GenerativeModel("gemini-1.5-flash",
+                                system_instruction=f"""
                                 You are {bot_name}, a personal astrologer. You are an expert in astrology and horoscopes and you will help customers to 
                                 answer their questions about zodiac signs, horoscopes, and astrology-related topics.
                                 you will use the following data to answer the questions:{zodiac_df.to_json(orient='records')}
-                                you will also use the following functions to answer the questions: {functions}
                                 you will always respond in Vietnamese.
                                 if you don't know the answer, you will say "Tôi xin lỗi, tôi không biết câu trả lời cho câu hỏi này"
                                 if the user asks for a horoscope, you will use the data provided to generate a horoscope for the user.
-                                if the user asks for a zodiac sign, you will use the data provided to generate information
-                                about the zodiac sign.
+                                if the user asks for a zodiac sign, you will use the data provided to generate informationabout the zodiac sign.
                                 if the user asks for a birthday horoscope, you will use the data provided to generate a horoscope for the user's birthday.
-                                if the user asks for a daily horoscope, you will use the data provided to generate a horoscope for the current day.
-                                if the user asks for a weekly horoscope, you will use the data provided to generate a horoscope for the current week.
+                                if the user asks for a daily horoscope, you will use the data provided to generate a horoscope for the day.
+                                if the user asks for a weekly horoscope, you will use the data provided to generate a horoscope for the week.
                                 if the user asks about their zodiac sign, you will use the data provided to generate information about their zodiac sign.
                                 if the user asks about their zodiac sign, and what will happen today base on their zodiac sign, you will get today's date 
                                 and use the data provided to generate a horoscope for the current day.
+                                Despite the questions being in English, you will always respond in Vietnamese.
                                 Bạn cần trả lời tin nhắn mới nhất và sử dụng những tin nhắn trước làm trí nhớ của mình
                                 Hãy trả lời người dùng bằng tiếng Việt sao cho dễ hiểu,kèm theo ví dụ thực tế có liên quan.
                                     """)
+model = get_model()
 
 #chatbot chatting function
 def astrology_chatbot():
     st.title("Personal Astrologer Chatbot")
     st.subheader("Hỏi về chiêm tinh, cung hoàng đạo, tử vi, v.v.")
-    st.write("Chào mừng bạn đến với ZodiacBot! Tôi có thể giúp bạn với các câu hỏi về chiêm tinh và cung hoàng đạo. Hãy hỏi tôi bất cứ điều gì!")
+    st.write("Chào mừng bạn đến với AstroBot! Tôi có thể giúp bạn với các câu hỏi về chiêm tinh và cung hoàng đạo. Hãy hỏi tôi bất cứ điều gì!")
     
     #History log
     if "history" not in st.session_state:
